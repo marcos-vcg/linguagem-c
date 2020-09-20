b11ad1020b7faf614f89299b73003e94eccb672f
Marcos Costa <64797599+marcos-vcg@users.noreply.github.com>
2020-09-19 21:59:09 -0300

    TiposPrimitivos

--- CDL/PI1/projeto.c
+++ CDL/PI1/projeto.c
@@ -3,12 +3,82 @@
 #include <stdlib.h>
 int main(int argc, char const *argv[])
 {
+	char pIp;
+	typedef struct {
+    	char ip[15];
+    	char pc[15]; 
+		char so[10];
+		char versao[10];
+		char responsavel[20]; 
+		char setor[15]; 
+	} cad;
+	cad comp[50];
+	int id = 10;
+	
+	
 	int opcao = Menu ();
-    SubMenu(opcao);
+    //SubMenu(opcao); 
+    
+    
+	switch (opcao) {
+		case 1: 	
+			printf("****Cadastrar um novo computador****\n");
+			id++;
+			cad comp[id];			
+			printf("Informe o IP da maquina: ");
+			scanf("%s%*c", &comp[id].ip);
+			printf("Informe o nome da maquina: ");
+			scanf("%s", &comp[id].pc);
+			printf("Informe o SO instalado na maquina: ");
+			scanf("%s", &comp[id].so);
+			printf("Informe a versao do SO instalada: ");
+			scanf("%s", &comp[id].versao);
+			printf("Informe o usuario responsavel pela maquina: ");
+			scanf("%s", &comp[id].responsavel);
+			printf("Em qual setor o PC esta instalado: ");
+			scanf("%s", &comp[id].setor);
+			printf("************************************\n");
+			printf("Cadastro do computador efetuado com sucesso!\n");
+			printf("************************************\n");
+			//imprimir(concat(comp[id].pc, comp[id].ip, comp[id].so, comp[id].versao, comp[id].responsavel, comp[id].setor));
+			printf("\n");
+			//Menu();
+			//return(comp[id]);
+			break;
+		case 2:
+			printf("****Consultar um computador****\n");
+			printf("Informe o IP do computador: ");
+			//scanf("%c", &pIp);
+			break;
+		case 3:
+			printf("****Alterar um computador****\n");
+			printf("Informe o IP do computador: ");
+			scanf("%f", &pIp);
+			break;
+		case 4:
+			printf("****Excluir um computador****\n");
+			printf("Informe o IP do computador: ");
+			scanf("%f", &pIp);
+			break;
+		case 5:
+			printf("****Listar todos os computadores****\n");
+			printf("A lista dos computadores e:\n");
+			printf("");
+			break;
+		case 6:
+			printf("****Gerar backup****\n");
+			printf("Gerando beckup:\n");
+			break;
+		case 9:
+			printf("****SAIR****\n");
+			printf("Obrigado e volte sempre!");
+			break;
+		default: printf("Opcao Invalida!");
+	}
 }
 
 
-int Menu (){
+/*int Menu (){
 	int opcao;
 	printf("****Bem-vindos ao sistema CADTI****\n");
     printf("********* Menu do sistema**********\n");
@@ -97,7 +167,7 @@ int SubMenu (int vl_opcao) {
 		default: printf("Opcao Invalida!");
 	}
     
-}
+}*/
 
 
 /*char imprimir(char str1[], char str2[], char str3[], char str4[], char str5[], char str6[]){
