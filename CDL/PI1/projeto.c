724c3d7abfbff167415a5df58c373e0faff2d4c8
Marcos Costa <64797599+marcos-vcg@users.noreply.github.com>
2020-10-25 22:00:57 -0300

    finalizacao

--- CDL/PI1/projeto.c
+++ CDL/PI1/projeto.c
@@ -7,49 +7,53 @@ FILE *arq, *bkp, *temp;
 
 
 void Cadastrar() {
+	
 	arq = fopen("dados.txt","a");
 	
 	if (arq == NULL){
-		printf("ERRO! O arquivo nao foi aberto!\n"); // Caso dê algum erro no momento de abertura do arquivo, aparece essa menssagem.
+		printf("ERRO! O arquivo nao foi aberto!\n"); // Caso de algum erro no momento de abertura do arquivo, aparece essa menssagem.
 	} else {
 	
 		char ip[20], pc[20], so[20], versao[20], responsavel[20], setor[20];
-		printf("****Cadastrar um novo computador****\n");
-		
-		printf("Informe o IP da maquina: ");
+  
+		printf(" ****** Cadastrar um novo computador *******\n");
+		printf(" *******************************************\n");	
+				
+		printf(" - Informe o IP da maquina: ");
 		scanf("%s", &ip);
 		
-		printf("Informe o nome da maquina: ");
+		printf(" - Informe o nome da maquina: ");
 		scanf("%s", &pc);
 		
-		printf("Informe o SO instalado na maquina: ");
+		printf(" - Informe o SO instalado na maquina: ");
 		scanf("%s", &so);
 		
-		printf("Informe a versao do SO instalada: ");
+		printf(" - Informe a versao do SO instalada: ");
 		scanf("%s", &versao);
 		
-		printf("Informe o usuario responsavel pela maquina: ");
+		printf(" - Informe o usuario responsavel: ");
 		scanf("%s", &responsavel);
 		
-		printf("Em qual setor o PC esta instalado: ");
+		printf(" - Em qual setor o PC esta instalado: ");
 		scanf("%s", &setor);
 		
 		fprintf(arq, "%s ; %s ; %s ; %s ; %s ; %s\n", ip, pc, so, versao, responsavel, setor);
 		fclose(arq);
 		
-	  	printf("\n");
-	  	printf("************************************\n");
-		printf("Cadastro do computador efetuado com sucesso!\n");
-		printf("************************************\n");
+	  	printf(" ********************************************\n");	  
+		printf(" Cadastro do computador efetuado com sucesso!\n");
+		printf(" ********************************************\n");
 		
-	  	sleep(3);
+		system("pause"); //faz com que o app espere um comando do usuario antes da proxima execucao.
   	}
 }
 
 void Consultar(){
+	
 	char busca[20];
-	printf("****Consultar um computador****\n");
-	printf("Informe o IP do computador: ");
+	printf("\n ********* Consultar um computador *********");
+	printf("\n *******************************************");
+	printf("\n - Informe o IP do computador: ");
 	scanf("%s", &busca);
 	
 	char ip[20], pc[20], so[20], versao[20], responsavel[20], setor[20], registro[200];
@@ -61,14 +65,14 @@ void Consultar(){
 		fscanf(arq, "%s ; %s ; %s ; %s ; %s ; %s", &ip, &pc, &so, &versao, &responsavel, &setor);
     
 	    if(strcmp(ip, busca) == 0){
-			printf("\n*******************************************");
-			printf("\nIP da maquina: %s", ip);
-			printf("\nNome da maquina: %s", pc);
-			printf("\nSO da maquina: %s", so);
-			printf("\nVersao do SO: %s", versao);
-			printf("\nUsuario responsavel: %s", responsavel);
-			printf("\nSetor que o PC esta instalado: %s", setor);
-			printf("\n*******************************************\n");
+			printf("\n *******************************************");
+			printf("\n - IP da maquina: %s", ip);
+			printf("\n - Nome da maquina: %s", pc);
+			printf("\n - SO da maquina: %s", so);
+			printf("\n - Versao do SO: %s", versao);
+			printf("\n - Usuario responsavel: %s", responsavel);
+			printf("\n - Setor que o PC esta instalado: %s", setor);
+			printf("\n *******************************************\n");
 			
 			cont++;
 	    }
@@ -82,20 +86,22 @@ void Consultar(){
   	}
   	
   	if(cont == 0) {
-  		printf("\n*******************************************");	
-	  	printf("\nEste IP nao esta cadastrado no sitema!");
-	  	printf("\n*******************************************\n");
+  		printf("\n *******************************************");	
+	  	printf("\n  - Este IP nao esta cadastrado no sitema!");
+	  	printf("\n *******************************************\n");
 	}
   	
   	fclose(arq);
-	system("pause"); //faz com que o app espere um comando do usuário antes da proxima execução.	
+	system("pause"); //faz com que o app espere um comando do usuario antes da proxima execucao.	
 }
 
 void Alterar(){
+	
 	char busca[20];
-	printf("\n\t****Alterar um computador****\n");
-	printf("\n*******************************************");
-	printf("\nInforme o IP do computador: ");
+	printf("\n");
+	printf(" ********** Alterar um computador **********\n");
+	printf(" *******************************************\n");
+	printf("\n Informe o IP do computador: ");
 	scanf("%s", &busca);
 	
 	char ip[20], pc[20], so[20], versao[20], responsavel[20], setor[20], texto[1001];
@@ -110,15 +116,16 @@ void Alterar(){
 		fscanf(arq, "%s ; %s ; %s ; %s ; %s ; %s", &ip, &pc, &so, &versao, &responsavel, &setor);
     
 	    if(!strcmp(ip, busca)){
-			printf("\n\t****IP Encontrado!****");
-			printf("\n*******************************************");
-			printf("\nIP da maquina: %s", ip);
-			printf("\nNome da maquina: %s", pc);
-			printf("\nSO da maquina: %s", so);
-			printf("\nVersao do SO: %s", versao);
-			printf("\nUsuario responsavel: %s", responsavel);
-			printf("\nSetor que o PC esta instalado: %s", setor);
-			printf("\n*******************************************\n");
+	    	printf("\n *******************************************");
+			printf("\n *             IP Encontrado!              *");
+			printf("\n *******************************************");
+			printf("\n  - IP da maquina: %s", ip);
+			printf("\n  - Nome da maquina: %s", pc);
+			printf("\n  - SO da maquina: %s", so);
+			printf("\n  - Versao do SO: %s", versao);
+			printf("\n  - Usuario responsavel: %s", responsavel);
+			printf("\n  - Setor que o PC esta instalado: %s", setor);
+			printf("\n *******************************************\n\n");
 			linha_selecionada = linha_atual; 
 			break;
 	    }
@@ -133,29 +140,27 @@ void Alterar(){
 		linha_atual += 1;
   	}
   	
-  	fclose(arq);
-  	
-  	
-	printf("****Atualizar cadastro do computador****\n");	
-	printf("Informe o novo IP da maquina: ");
-	scanf("%s", &ip);
-	printf("Informe o novo nome da maquina: ");
-	scanf("%s", &pc);
-	printf("Informe o novo SO instalado na maquina: ");
-	scanf("%s", &so);
-	printf("Informe a nova versao do SO instalada: ");
-	scanf("%s", &versao);
-	printf("Informe o novo usuario responsavel pela maquina: ");
-	scanf("%s", &responsavel);
-	printf("Em qual novo setor o PC esta instalado: ");
-	scanf("%s", &setor);
-  	
-  	
+  	fclose(arq);	
 	arq = fopen("dados.txt","r");
 	temp = fopen("temp.txt","a");
 	
 	linha_atual = 1;
 	if(linha_selecionada != 0){
+		
+		printf(" *****Atualizar cadastro do computador******\n");	 	
+		printf(" - Informe o novo IP da maquina: ");
+		scanf("%s", &ip);
+		printf(" - Informe o novo nome da maquina: ");
+		scanf("%s", &pc);
+		printf(" - Informe o novo SO instalado na maquina: ");
+		scanf("%s", &so);
+		printf(" - Informe a nova versao do SO instalada: ");
+		scanf("%s", &versao);
+		printf(" - Informe o novo usuario responsavel: ");
+		scanf("%s", &responsavel);
+		printf(" - Em qual novo setor o PC esta instalado: ");
+		scanf("%s", &setor);
+		
   		while (fgets(texto, 1001, arq) != NULL){	
 			if(linha_atual != linha_selecionada){
 				fputs (texto,temp);	
@@ -171,19 +176,18 @@ void Alterar(){
 		remove("dados.txt");
 		rename("temp.txt", "dados.txt");
 		
-		printf("\n\nExclusao do PC efetuada com sucesso!");
-  		printf("\n*******************************************\n");	  
-    	
+  		printf("*******************************************");	  
+    	printf("\n     Atualizacao efetuada com sucesso!\n\n");
+    		
 	} else {
 		printf("\n\t****IP nao Encontrado!****");
-		printf("\n\nExclusao do PC nao efetuada!");
   		printf("\n*******************************************\n");
   		
   		fclose(arq);
 		fclose(temp);
 	}    
 
-	system("pause");
+	system("pause"); //faz com que o app espere um comando do usuario antes da proxima execucao.
 }
 
 void Excluir(){
@@ -240,22 +244,22 @@ void Excluir(){
 		remove("dados.txt");
 		rename("temp.txt", "dados.txt");
 		
-		printf("\n\nExclusao do PC efetuada com sucesso!");
+		printf("\n\nExclusao efetuada com sucesso!");
   		printf("\n*******************************************\n");	  
     	
 	} else {
 		printf("\n\t****IP nao Encontrado!****");
-		printf("\n\nExclusao do PC nao efetuada!");
   		printf("\n*******************************************\n");
   		
   		fclose(arq);
 		fclose(temp);
 	}    
 
-	sleep(3);
+	system("pause"); //faz com que o app espere um comando do usuario antes da proxima execucao.
 }
 
 void Listar(){
+	
 	printf("****Listar todos os computadores****\n");
 	printf("A lista dos computadores cadastrados e:\n");
 
@@ -294,6 +298,7 @@ void Listar(){
 }
 
 void Backup(){
+	
 	printf("\n\t****Gerar backup****\n");
 	printf("\n*******************************************");
 	
@@ -316,29 +321,36 @@ void Backup(){
 	
 	fclose(arq);
 	fclose(bkp);
+	
+	system("pause"); //faz com que o app espere um comando do usuario antes da proxima execucao.
 }
 
 void Sair(){
-	printf("****SAIR****\n");
-	printf("Obrigado e volte sempre!");
+	
+	printf("\n ************ SAIR ************");
+	printf("\n ******************************\n");
+	printf(" - Obrigado e volte sempre!\n");
+	
+	//system("pause"); //faz com que o app espere um comando do usuario antes da proxima execucao.
 }
 
 
 void Menu(){
+	
 	int opcao;
   	do {
 	  	system("clear||cls");
-		printf("****Bem-vindos ao sistema CADTI****\n");
-	    printf("********* Menu do sistema**********\n");
-	    printf("1 - Cadastrar um novo computador\n");
-	    printf("2 - Consultar um computador\n");
-	    printf("3 - Alterar um computador\n");
-	    printf("4 - Excluir um computador\n");
-	    printf("5 - Listar todos os computadores\n");
-	    printf("6 - Gerar backup\n");
-	    printf("0 - Sair\n");
-	    printf("************************************\n"); 
-		printf("Digite aqui o que deseja fazer: ");
+		printf(" ****Bem-vindos ao sistema CADTI*****\n");
+	    printf(" ********* Menu do sistema***********\n");
+	    printf(" * 1 - Cadastrar um novo computador *\n");
+	    printf(" * 2 - Consultar um computador      *\n");
+	    printf(" * 3 - Alterar um computador        *\n");
+	    printf(" * 4 - Excluir um computador        *\n");
+	    printf(" * 5 - Listar todos os computadores *\n");
+	    printf(" * 6 - Gerar backup                 *\n");
+	    printf(" * 0 - Sair                         *\n");
+	    printf(" ************************************\n"); 
+		printf(" Digite aqui o que deseja fazer: ");
 		scanf("%d", &opcao);
 		system("clear||cls");
 
